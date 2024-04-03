4c474aabef7f295945b972cf4a47346ed6ab0e6e
VagosTurbo <semanco.boris@gmail.com>
2024-04-03 22:13:32 +0200

    first prject finished

---
+++ proj1/ija2023-ukol1/ija/ija2023/homework1/common/Environment.java
@@ -0,0 +1,16 @@
+package ija.ija2023.homework1.common;
+
+public interface Environment {
+
+    boolean addRobot(Robot robot);
+    boolean createObstacleAt(int row, int col);
+
+    boolean obstacleAt(int row, int col);
+
+    boolean obstacleAt(Position p);
+
+    boolean robotAt(Position p);
+
+    boolean containsPosition(Position pos);
+}
+
